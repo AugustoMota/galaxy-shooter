a320be1902dfbef52025a135291e1f44ca375b11
AugustoMota <augustomota180798@gmail.com>
2023-02-08 14:51:59 -0300

    Movimento para cima e para o lado do personagem

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var moveX= Input.GetAxis("Horizontal");
+        var moveY= Input.GetAxis("Vertical"); 
+        transform.Translate(speed * Vector3.right * Time.deltaTime * moveX);
+        transform.Translate(speed * Vector3.up * Time.deltaTime * moveY);
+    }
+}
