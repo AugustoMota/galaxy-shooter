4d8bd4137057fca7bab0401b34649cf64f32fccb
AugustoMota <augustomota180798@gmail.com>
2023-02-09 14:26:31 -0300

    Limita espaço player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// x 9.3080  -9.3080
+// y 6.193606 -3.688
 public class Player : MonoBehaviour
 {
     public float speed = 1.0f;
@@ -20,5 +22,36 @@ public class Player : MonoBehaviour
         var moveY= Input.GetAxis("Vertical"); 
         transform.Translate(speed * Vector3.right * Time.deltaTime * moveX);
         transform.Translate(speed * Vector3.up * Time.deltaTime * moveY);
+
+        if( transform.position.x > 9.0f)
+        {
+            var newposition = transform.position;
+            newposition.x = 9.0f;
+            transform.position = newposition;
+        }
+
+        if(transform.position.x < -9.0f)
+        {
+            var newposition = transform.position;
+            newposition.x = -9.0f;
+            transform.position = newposition;
+        }
+
+        if (transform.position.y > 6.0f)
+        {
+            var newposition = transform.position;
+            newposition.y = 6.0f;
+            transform.position = newposition;
+        }
+
+        if (transform.position.y < -4.0f)
+        {
+            var newposition = transform.position;
+            newposition.y = -4.0f;
+            transform.position = newposition;
+        }
+
+
+
     }
 }
